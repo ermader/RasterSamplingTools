13208b71a002cbe69bb2949bfcfd07f69a81602f
Eric Mader <emader@gmx.us>
2021-09-02 08:47:28 -1000

    Add installer shell script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# get sources
+git clone https://bitbucket.org/LindenbergSW/FontDocTools
+git clone https://github.com/ermader/UnicodeData
+git clone https://github.com/ermader/TestArguments
+git clone https://github.com/ermader/PathLib
+
+# do the installs
+
+for dir in FontDocTools UnicodeData TestArguments PathLib RasterSamplingTools ; do
+  cd dir
+  # echo installing $dir
+  python setup.py install
+  cd ..
+done
