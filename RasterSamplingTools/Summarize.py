ed515a0d62aab281dabe117933182b8eb7cbd377
Eric Mader <emader@gmx.us>
2021-10-09 16:07:45 -1000

    Remove text file writing code. Display 4 decimal places for R2

--- RasterSamplingTools/Summarize.py
+++ RasterSamplingTools/Summarize.py
@@ -33,8 +33,8 @@ class SummarizeArgs(CommandLineArgs):
     options = [
         CommandLineOption("input", None, lambda a: a.nextExtra("input file"), "inputFile", None),
         CommandLineOption("output", None, lambda a: a.nextExtra("output file"), "outputFile", None, required=False),
-        CommandLineOption("widthFields", lambda s, a: CommandLineOption.valueFromDict(s.widthFieldDict, a, "width fields spec"), lambda a: a.nextExtra("width fields"), "widthFields", "median", required=False),
-        CommandLineOption("excel", None, True, "excel", False, required=False),
+        # CommandLineOption("widthFields", lambda s, a: CommandLineOption.valueFromDict(s.widthFieldDict, a, "width fields spec"), lambda a: a.nextExtra("width fields"), "widthFields", "median", required=False),
+        # CommandLineOption("excel", None, True, "excel", False, required=False),
     ]
 
     def __init__(self):
@@ -70,6 +70,7 @@ def main():
     try:
         args = SummarizeArgs()
         args.processArguments(argumentList)
+        args.widthFields = SummarizeArgs.widthFieldDict["most"]  # need a better way to do this...
     except ValueError as error:
         print(programName + ": " + str(error), file=stderr)
         exit(1)
@@ -77,20 +78,17 @@ def main():
     # font = ctFont("Calibri", 11)
 
     outdb = OutputDatabase(args.inputFile)
-    # outFile = open(args.outputFile, "w") if args.outputFile else stdout
-    outFile = None
     widthFields = args.widthFields
     fitResultFields = ["stroke_angle", "r_squared"]
 
-    if args.excel:
-        wb = Workbook()
-        ws = wb.active
-        fieldNames = ["ps_name", "tested glyphs", "ignored glyphs"]
-        fieldNames.extend(widthFields)
-        fieldNames.extend(["range", "range as % of median", "min angle", "median angle", "mean angle", "max angle", "angle range", "range as % of median", "min R\u00B2", "median R\u00B2", "mean R\u00B2", "max R\u00B2", "R\u00B2 range", "range as % of median"])
+    wb = Workbook()
+    ws = wb.active
+    fieldNames = ["ps_name", "tested glyphs", "ignored glyphs"]
+    fieldNames.extend(widthFields)
+    fieldNames.extend(["range", "range as % of median", "min angle", "median angle", "mean angle", "max angle", "angle range", "range as % of median", "min R\u00B2", "median R\u00B2", "mean R\u00B2", "max R\u00B2", "R\u00B2 range", "range as % of median"])
 
-        for column, label in enumerate(fieldNames):
-            ws.cell(row=1, column=column+1, value=label).alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
+    for column, label in enumerate(fieldNames):
+        ws.cell(row=1, column=column+1, value=label).alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
 
     rowNumber = 2
     # maxWidth = 0
@@ -104,10 +102,7 @@ def main():
 
         # maxWidth = max(maxWidth, stringWidth(psName, font))
 
-        if args.excel:
-            row = [psName]
-        else:
-            outFile.write(f"{psName}\t")
+        row = [psName]
 
         for result in testResults.values():
             widthResults = result.get("widths", None)
@@ -125,54 +120,46 @@ def main():
         if haveWidths:
             means = [round(statistics.mean(widths[wf]), 1) for wf in widthFields]
 
-            fitMeans = {frf: round(statistics.mean(fits[frf]), 1) for frf in fitResultFields}
-            fitMedians = {frf: round(statistics.median(fits[frf]), 1) for frf in fitResultFields}
-            fitMins = {frf: round(min(fits[frf]), 1) for frf in fitResultFields}
-            fitMaxs = {frf: round(max(fits[frf]), 1) for frf in fitResultFields}
+            fitMeans = {frf: statistics.mean(fits[frf]) for frf in fitResultFields}
+            fitMedians = {frf: statistics.median(fits[frf]) for frf in fitResultFields}
+            fitMins = {frf: min(fits[frf]) for frf in fitResultFields}
+            fitMaxs = {frf: max(fits[frf]) for frf in fitResultFields}
 
-            if args.excel:
-                row.extend([goodGlyphCount, len(testResults) - goodGlyphCount])
-                row.extend(means)
-                ws.append(row)
-                ws.cell(row=rowNumber, column=10, value=rangeFormula(rowNumber, 4, 9))
-                ws.cell(row=rowNumber, column=11, value=percentFormula(rowNumber, 6, 10)).number_format = "0.0%"
+            widthColumn = 4
+            row.extend([goodGlyphCount, len(testResults) - goodGlyphCount])
+            row.extend(means)
+            ws.append(row)
+            ws.cell(row=rowNumber, column=widthColumn+4, value=rangeFormula(rowNumber, widthColumn, widthColumn+3))
+            ws.cell(row=rowNumber, column=widthColumn+5, value=percentFormula(rowNumber, widthColumn+1, widthColumn+4)).number_format = "0.0%"
 
-                angleColumn = 12
-                ws.cell(row=rowNumber, column=angleColumn, value=fitMins["stroke_angle"])
-                ws.cell(row=rowNumber, column=angleColumn+1, value=fitMedians["stroke_angle"])
-                ws.cell(row=rowNumber, column=angleColumn+2, value=fitMeans["stroke_angle"])
-                ws.cell(row=rowNumber, column=angleColumn+3, value=fitMaxs["stroke_angle"])
-                ws.cell(row=rowNumber, column=angleColumn+4, value=rangeFormula(rowNumber, angleColumn, angleColumn+3))
-                ws.cell(row=rowNumber, column=angleColumn+5, value=percentFormula(rowNumber, angleColumn+1, angleColumn+4)).number_format = "0.0%"
+            angleColumn = widthColumn+6
+            ws.cell(row=rowNumber, column=angleColumn, value=fitMins["stroke_angle"]).number_format = "0.0"
+            ws.cell(row=rowNumber, column=angleColumn+1, value=fitMedians["stroke_angle"]).number_format = "0.0"
+            ws.cell(row=rowNumber, column=angleColumn+2, value=fitMeans["stroke_angle"]).number_format = "0.0"
+            ws.cell(row=rowNumber, column=angleColumn+3, value=fitMaxs["stroke_angle"]).number_format = "0.0"
+            ws.cell(row=rowNumber, column=angleColumn+4, value=rangeFormula(rowNumber, angleColumn, angleColumn+3)).number_format = "0.0"
+            ws.cell(row=rowNumber, column=angleColumn+5, value=percentFormula(rowNumber, angleColumn+1, angleColumn+4)).number_format = "0.0%"
 
-                r2Column = angleColumn + 6
-                ws.cell(row=rowNumber, column=r2Column, value=fitMins["r_squared"])
-                ws.cell(row=rowNumber, column=r2Column+1, value=fitMedians["r_squared"])
-                ws.cell(row=rowNumber, column=r2Column+2, value=fitMeans["r_squared"])
-                ws.cell(row=rowNumber, column=r2Column+3, value=fitMaxs["r_squared"])
-                ws.cell(row=rowNumber, column=r2Column+4, value=rangeFormula(rowNumber, r2Column, r2Column+3))
-                ws.cell(row=rowNumber, column=r2Column+5, value=percentFormula(rowNumber, r2Column+1, r2Column+4)).number_format = "0.0%"
-            else:
-                outFile.write(f"{', '.join(means)}\n")
+            r2Column = angleColumn + 6
+            ws.cell(row=rowNumber, column=r2Column, value=fitMins["r_squared"]).number_format = "0.0000"
+            ws.cell(row=rowNumber, column=r2Column+1, value=fitMedians["r_squared"]).number_format = "0.0000"
+            ws.cell(row=rowNumber, column=r2Column+2, value=fitMeans["r_squared"]).number_format = "0.0000"
+            ws.cell(row=rowNumber, column=r2Column+3, value=fitMaxs["r_squared"]).number_format = "0.0000"
+            ws.cell(row=rowNumber, column=r2Column+4, value=rangeFormula(rowNumber, r2Column, r2Column+3)).number_format = "0.0000"
+            ws.cell(row=rowNumber, column=r2Column+5, value=percentFormula(rowNumber, r2Column+1, r2Column+4)).number_format = "0.0%"
         else:
-            if args.excel:
-                # ws.cell(row=rowNumber, column=1, value=psName)
-                # ws.cell(row=rowNumber, column=2, value=-999.9)
-                # ws.append([psName, goodGlyphCount, len(testResults) - goodGlyphCount, -9999.9])
-                ws.append([psName, goodGlyphCount, len(testResults) - goodGlyphCount])
-            else:
-                outFile.write("-999\n")
+            # ws.cell(row=rowNumber, column=1, value=psName)
+            # ws.cell(row=rowNumber, column=2, value=-999.9)
+            # ws.append([psName, goodGlyphCount, len(testResults) - goodGlyphCount, -9999.9])
+            ws.append([psName, goodGlyphCount, len(testResults) - goodGlyphCount])
 
         rowNumber += 1
 
-    if args.excel:
-        # ws.column_dimensions["A"].bestFit = True
-        ws.column_dimensions["A"].width = 30
-        # ws.column_dimensions["A"].auto_size = True
+    # ws.column_dimensions["A"].bestFit = True
+    # ws.column_dimensions["A"].auto_size = True
+    ws.column_dimensions["A"].width = 30  # maxWidth * .15?
 
-        wb.save(args.outputFile)
-    # if args.outputFile:
-    #     outFile.close()
+    wb.save(args.outputFile)
 
 if __name__ == "__main__":
     main()
